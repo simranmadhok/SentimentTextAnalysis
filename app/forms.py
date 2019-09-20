9d7abd619952002f9edf71afd6412f8fbcd135bd
simranmadhok <madhok.simran8@gmail.com>
2019-09-20 11:00:00 +0530

    Text analysis tool

--- app/forms.py
+++ app/forms.py
@@ -1,9 +1,10 @@
 from flask_wtf import FlaskForm
 from wtforms import BooleanField, TextAreaField
 from wtforms.validators import DataRequired
+from wtforms.widgets import TextArea
 
 
 class InputTextForm(FlaskForm):
-    inputText = TextAreaField(validators=[DataRequired()])
+    inputText = TextAreaField(validators=[DataRequired()], render_kw={"rows": 18, "cols": 70})
     ignoreCase = BooleanField('ignore case', default=True)
     ignoreStopWords = BooleanField('ignore stopwords', default=True)    
--- app/routes.py
+++ app/routes.py
@@ -2,28 +2,29 @@ from app import app
 from flask import render_template, request, url_for
 from app.forms import InputTextForm
 from .text_analyser import TextAnalyser
+import json
 
 @app.route('/')
 @app.route('/home')
 def home():
     return render_template('home.html')
 
-@app.route('/textminer', methods=['GET', 'POST'])
+@app.route('/textminer', methods=('GET', 'POST'))
 def textminer():
-    if request.method == 'GET':
-        input_text_form = InputTextForm()
-        return render_template('textminer.html', form = input_text_form, miningComplete = False)
-    elif request.method == 'POST':
-        input_text_form = InputTextForm(request.POST)
-        if input_text_form.validate_on_submit():
-            userText = input_text_form.inputText.data
-            myText = TextAnalyser(userText, 'EN')
-            myText.preprocessText(lowercase = input_text_form.ignoreCase.data,
-                        removeStopWords = input_text_form.ignoreStopWords.data)
-            return render_template('textminer.html',
-                         numChars = myText.length(),
-                         numTokens = myText.getTokens(),
-                         uniqueTokens =myText.uniqueTokens(),
-                         commonWords = myText.getMostCommonWords(10),
-                         miningComplete = True)
-
+    input_text_form = InputTextForm()
+    if input_text_form.validate_on_submit():
+        userText = input_text_form.inputText.data
+        myText = TextAnalyser(userText, 'EN')
+        myText.preprocessText(lowercase = input_text_form.ignoreCase.data,
+                    removeStopWords = input_text_form.ignoreStopWords.data)
+        commonWords = myText.getMostCommonWords(10)
+        commonWords.insert(0, ('Common Word', 'Frequency'))
+        data = [list(elem) for elem in commonWords]
+        return render_template('textminer.html',
+                        form = input_text_form,
+                        numChars = myText.length(),
+                        numTokens = myText.getTokens(),
+                        uniqueTokens =myText.uniqueTokens(),
+                        data = data,
+                        miningComplete = True)
+    return render_template('textminer.html', form = input_text_form, miningComplete = False)
